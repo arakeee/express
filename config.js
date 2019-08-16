be1190b7ea96fdccd7e8540eb27e39c2eb103965
Daniel <arak.gmail.com>
2019-08-16 19:18:19 +0200

    admin login news

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://admin:3yxEZgZfZhMxK2FK@cluster0-7lpfn.mongodb.net/test?retryWrites=true&w=majority',
+    keySession: ['TWOJKLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+}
